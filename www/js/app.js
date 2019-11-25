d39939027f8d2731254df63a2b7001fab03d9501
Glint96 <esysdev@tensi.org>
2019-11-25 21:10:42 +0900

    Project's Initial Commit

--- www/js/app.js
+++ www/js/app.js
@@ -3,98 +3,204 @@
 
       //ニフクラのsnsData取得
       var SNSData = ncmb.DataStore("SNSData");
+
+      //ニフクラのアカウントのアイコンとお気に入り投稿データ取得
+      var acountData = ncmb.DataStore("acountData");
+      
+      //ログイン状態の判断フラグ
+      var login_f = false;
       
       //画面遷移処理一覧
-       window.fn = {};
+      window.fn = {};
+      //サイドメニューの表示方向処理
+      window.fn.toggleMenu = function () {
+        document.getElementById('appSplitter').left.toggle();
+      };
 
-        window.fn.toggleMenu = function () {
-          document.getElementById('appSplitter').left.toggle();
-        };
+      //サイドメニューの閉じる処理？
+      window.fn.loadView = function (index) {
+        document.getElementById('appTabbar').setActiveTab(index);
+        document.getElementById('sidemenu').close();
+      };
 
-        window.fn.loadView = function (index) {
-          document.getElementById('appTabbar').setActiveTab(index);
-          document.getElementById('sidemenu').close();
-        };
+      //不明　未使用
+      window.fn.loadLink = function (url) {
+        window.open(url, '_blank');
+      };
 
-        window.fn.loadLink = function (url) {
-          window.open(url, '_blank');
-        };
+      //ページ遷移
+      window.fn.pushPage = function (page) {
+          document.getElementById('appNavigator').pushPage(page);
+      };
 
-        window.fn.pushPage = function (page) {
-           document.getElementById('appNavigator').pushPage(page);
-        };
-        
-        //mailsoushin.htmlからlogin.htmlに戻る画面遷移をバックボタン対応にしたい
-        /* document.addEventListener('destroy',function(e){
-          if(e.target.matches('#soushin')){
-            //console.log(document.getElementById("appNavigator").topPage);
-            var page = document.getElementById("appNavigator");
-            var pages = page.pages;
-            //console.log(pages);
-            pages[pages.length-2].remove();
-          }
-        });*/
+      //ページスタックリセット処理
+      window.fn.resetToPage = function (page) {
+        document.getElementById('appNavigator').resetToPage(page);
+      };
+
+      //ページ置き換え
+      window.fn.replacePage = function (page) {
+        document.getElementById('appNavigator').replacePage(page);
+      };
+
+      // ぺージを一番上までスクロールする処理
+      // 未動作
+      function pageUp(){
+        document.getElementById('snsitiran').scrollTo(0,0);
+      }
 
          //端末の準備ができたら呼び出し
-  document.addEventListener("deviceready", onDiviceReady, false);
-  function onDiviceReady(){
-    //ログアウトしなかった時の次回自動ログイン処理
-    var currentUser = ncmb.User.getCurrentUser();
-            if (currentUser){
-        //    alert(JSON.stringify(currentUser));
-                ncmb.User.fetch()
-                    //ログイン成功
-                    .then(function(results){
-                        console.log("currentUserName is " + currentUser.get("userName"));
-                        //index.htmlのサイドメニューの文字切り替え
-                        document.getElementById("login_button").innerHTML = "ログアウト";
-                        login_f = true;
-                        //acount呼び出し
-                        acount();
-                    })
-                    //ログイン失敗
-                    .catch(function(err){
-                        ncmb.User.logout();
-                    });
+      document.addEventListener("deviceready", onDiviceReady, false);
+      function onDiviceReady(){
+        //ログアウトしなかった時の次回自動ログイン処理
+        var currentUser = ncmb.User.getCurrentUser();
+              // ログアウトしていないなら
+                if (currentUser){
+            //    alert(JSON.stringify(currentUser));
+                    ncmb.User.fetch()
+                        //ログイン成功
+                        .then(function(results){
+                            console.log("currentUserName is " + currentUser.userName);
+                            //index.htmlのサイドメニューの文字切り替え
+                            document.getElementById("login_button").innerHTML = "ログアウト";
+                            login_f = true;
+                            //acount呼び出し
+                            acount();
+                        })
+                        //ログイン失敗
+                        .catch(function(err){
+                            ncmb.User.logout();
+                        });
 
-            } else {
-                //未ログインの場合
-            }
-      }     
+                } else {
+                    //未ログインの場合
+                }
+
+          }     
 
       function acount(){
         currentUser = ncmb.User.getCurrentUser();
         //index.htmlとmypage.htmlのアカウント名を使用ユーザに書き換え
-        document.getElementById("ind_acount_name").innerHTML = currentUser.get("userName");
+        document.getElementById("ind_acount_name").innerHTML = currentUser.userName;
+        document.getElementById("ind_acount_Icon").style.visibility = "visible";
         //ニフクラのsnsData取得
         var SNSData = ncmb.DataStore("SNSData");
         //投稿数取得
         SNSData
-        .equalTo("userName",currentUser.get("userName"))
+        .equalTo("userName",currentUser.userName)
         .count()
         .fetchAll()
         .then(function(res){ 
           var countpost = res.count + "post";
           document.getElementById("ind_acount_post").innerHTML = countpost;
+
+          acountData
+          .equalTo("username",currentUser.username)
+          .fetch()
+          .then(function(res){
+            //console.log(JSON.stringify(res));
+            //アイコンが設定されてなければデフォルトアイコン表示
+            if(res.userIcon === "defaultIcon.png"){
+              document.getElementById("ind_acount_Icon").src = "photo/defaultIcon.png"
+            }else{
+              var reader = new FileReader();
+              downloadImage(res[0].userIcon,reader);
+              //画像を表示
+              reader.onload = function(e) {
+                  var dataUrl = reader.result;
+                  getPicture(dataUrl,res[0].userIcon);
+                  document.getElementById("ind_acount_Icon").src;
+              }
+            }
+          });
         },function(err){
           console.log(err);
         });
       }   
 
-          //ハートを押した時の処理
-      var like = function(num) {
-        if (document.getElementById("button-post-like-"+num).classList.contains("like")) {
-          document.getElementById("button-post-like-"+num).classList.remove("md-favorite","like");
-          document.getElementById("button-post-like-"+num).classList.add("md-favorite-outline");
+  // ダイアログ表示
+  var showDialog = function(edit) {
+    //console.log(edit);
+    switch(edit){
+      case "name":
+        var dialog = document.getElementById('name-dialog');
+        if (dialog) {
+          dialog.show();
         } else {
-          document.getElementById("button-post-like-"+num).classList.remove("md-favorite-outline");
-          document.getElementById("button-post-like-"+num).classList.add("md-favorite","like");
-          //document.getElementById("post-like-"+num).style.opacity = 1;
-
-            //ハートを押したときに一定時間大きなハートを出す処理 未使用
-          /*setTimeout(function(){
-            document.getElementById("post-like-"+num).style.opacity = 0;
-          }, 600);*/
+          ons.createElement('name-dialog.html', { append: true })
+            .then(function(dialog) {
+              dialog.show();
+            });
         }
-      }
+        break;
+      case "unsub":
+        var dialog = document.getElementById('unsub-dialog');
+        if (dialog) {
+          dialog.show();
+        } else {
+          ons.createElement('unsub-dialog.html', { append: true })
+            .then(function(dialog) {
+              dialog.show();
+            });
+        }
+        break;
+    case "mail":
+        var dialog = document.getElementById('mail-dialog');
+        if (dialog) {
+          dialog.show();
+        } else {
+          ons.createElement('mail-dialog.html', { append: true })
+            .then(function(dialog) {
+              dialog.show();
+            });
+        }
+        break;
+    }
+  };
 
+  // ダイアログ非表示
+  var hideDialog = function(id) {
+    //console.log(id);
+    document
+      .getElementById(id + "-dialog")
+      .hide();
+  };
+
+    
+   var showpop_f = false; //ポップバー管理フラグ
+    // ポップバー表示処理
+    var showPopover = function(target,num,page) {
+      //  編集や削除処理のために数字引き渡し
+      sns_num = num;
+      // ポップバーが表示されていなければ表示
+      // されていれば非表示
+      if(!showpop_f){
+      document
+        .getElementById('popover'+page)
+        .show(target,num);
+        showpop_f = true;
+      }else{
+        hidePopover();
+      }
+    };
+    
+    // ポップバー非表示処理
+    var hidePopover = function() {
+      document
+        .getElementById('popover1')
+        .hide();
+      document
+        .getElementById('popover2')
+        .hide();
+
+      showpop_f = false;
+    };
+
+    // popoverにつけたcacelable属性で閉じた時用
+    // （hidepopover()を使わずに閉じた時）
+    document.addEventListener("prehide",function(e){
+      //console.log(e.target.id);
+      if(e.target.id === ("popover1")||e.target.id ===("popover2")){
+        showpop_f = false;
+      }
+    });
--- www/js/sns.js
+++ www/js/sns.js
@@ -5,10 +5,23 @@
   var sns_post      //snshyouji.htmlに渡す数
   var sns_num       //SNSのpopverの動作管理番号
 
-      //ページ読込時に実行
+  var destroy_f = false; //マイページの削除機能管理
+
+  //ページ読込時に実行
   document.addEventListener('init',function(e){
       if(e.target.matches('#snsitiran')){
         snsCount();
+      }
+  });
+
+  // マイページを閉じた＆投稿を削除した場合、更新する
+  //destroy_fはAPIリクエスト削減のため使用
+  document.addEventListener("destroy",function(e){
+      if(e.target.matches('#profile-page')&&destroy_f){
+        // 現在表示されている投稿を削除
+        document.getElementById('ind_sns-list').refresh();
+        snsCount();
+        destroy_f = false;
       }
   });
 
@@ -32,22 +45,45 @@
 
 //SNSの動的作成
 function snsMake(res,snsList,page){
+  if(login_f){
+    if(page === "sns"){
+      // ユーザーのお気に入り情報獲得
+      var currentUser = ncmb.User.getCurrentUser();
+      var likeItem;
+      acountData
+      .equalTo("userName",currentUser.userName)
+      .fetch()
+      .then(function(res){
+        likeItem = res.likeSns;
+      });
+    }
+  }
   snsList.delegate = {
       createItemContent: function(i) {
         //投稿物を取得
         sns_res = res;
-        var object = res[res.length-(i+1)];
-  
-        var userName  = object.get("userName");
-        var Date      = object.get("createDate");
-        var text      = object.get("text");
+        var object = res[res.length-(i+1)];       //SNSDataのデータ配列
+
+        /*
+        var userName  = object.get("userName");   //ユーザーネーム
+        var Date      = object.get("createDate"); //投稿日
+        var text      = object.get("text");       //テキスト
+        */
+
+        //日付の文字列を切り出して表示
+        var str = object.createDate;
+        var date = str.substring(0,10);
+        var hour = parseInt(str.substring(11,13),10) + 9;
+        var min = str.substring(14,16);
+        var creDate = date +" "+ hour +":"+ min;
+        
+
 
         var reader = new FileReader();
 
         //写真が配列に保存してあるか判断
         var flg = false;
         for(var u=picture_name.length-1; 0<=u; u--){
-          console.log(u);
          if(res[res.length-(u+1)].picture===object.picture){
             flg = true;
           }
@@ -65,66 +101,110 @@ function snsMake(res,snsList,page){
          }
 
         if(page === "sns"){
+          var like_f = false;
+            if(login_f){
+            // お気に入り処理してある投稿にlike（css）付与
+            var objId = object.get("objectId");
+            for(var u=likeItem.length-1; 0<=u; u--){
+              if(likeItem[u] === objId){
+                like_f = true;
+                break;
+              }
+            }
+          }
           //SNSの型
           //    \（円）マークで改行
-        return ons.createElement('<ons-card class="post">\
-                                    <div id="ind_obj'+i+'" class="obj_post">'+object.get("objectId")+'</div>\
-                                    <ons-list-item>\
-                                      <div class="left">\
-                                          <img class="list-item__thumbnail" src="photo/sofa.png" >\
-                                        </div>\
-                                        <div class="center">\
-                                          <div class="list-item__title"><b>'+userName+'</b></div>\
-                                          <div class="my_post-time">'+Date+'</div>\
-                                      </div>\
-                                      <div class="right">\
-                                        <span class="corner-button" modifier="quiet" onclick="showPopover(this,'+i+')">\
-                                          <ons-icon icon="md-more-vert"></ons-icon>\
-                                        </span>\
-                                      </div>\
-                                    </ons-list-item>\
-                                    <div style="text-align: center; position: relative;">\
-                                      <img class="post-image" id="ind_img'+i+'" src="photo/cameraIcon.png" onclick="gosnshyouji('+i+')">\
-                                    </div>\
-                                    <ons-list-item class="post-button-bar" modifier="nodivider">\
-                                      <div class="center" style="padding-top: 0px">\
-                                      </div>\
-                                      <div class="right corner-button bookmark">\
-                                        <ons-button class="post-button" modifier="quiet" onclick="like('+ i +')"><ons-icon icon="md-favorite-outline" id="button-post-like-'+ i +'" ></ons-icon></ons-button>\
-                                      </div>\
-                                    </ons-list-item>\
-                                    <div class="post-like-info"><b></b> </div>\
-                                    <div class="post-caption"> '+ text +' </div>\
-                                  </ons-card>');
-        }else if(page === "mysns"){
+          if(!like_f){
           return ons.createElement('<ons-card class="post">\
+                                      <div id="ind_obj'+i+'" class="obj_post">'+object.get("objectId")+'</div>\
+                                      <ons-list-item>\
+                                        <div class="left">\
+                                            <img class="list-item__thumbnail" src="photo/defaultIcon.png">\
+                                          </div>\
+                                          <div class="center">\
+                                            <div class="list-item__title"><b>'+object.get("userName")+'</b></div>\
+                                            <div class="my_post-time">'+creDate+'</div>\
+                                        </div>\
+                                        <div class="right">\
+                                          <span class="corner-button" modifier="quiet" onclick="showPopover(this,'+i+','+1+')">\
+                                            <ons-icon icon="md-more-vert"></ons-icon>\
+                                          </span>\
+                                        </div>\
+                                      </ons-list-item>\
+                                      <div style="text-align: center; position: relative;">\
+                                        <img class="post-image" id="ind_img'+i+'" src="photo/cameraIcon.png" onclick="gosnshyouji('+i+')">\
+                                      </div>\
+                                      <ons-list-item class="post-button-bar" modifier="nodivider">\
+                                        <div class="center" style="padding-top: 0px" onclick="gosnshyouji('+i+')">\
+                                          <div class="post-caption" style="overflow:hidden;height:30px"> '+ object.get("text") +'\
+                                        </div>\
+                                        </div>\
+                                        <div class="right corner-button bookmark">\
+                                          <ons-button class="post-button" modifier="quiet" onclick="like('+ i +')"><ons-icon icon="md-favorite-outline" id="button-post-like-'+ i +'" ></ons-icon></ons-button>\
+                                        </div>\
+                                      </ons-list-item>\
+                                    </ons-card>');
+          }else{
+            return ons.createElement('<ons-card class="post">\
+                                      <div id="ind_obj'+i+'" class="obj_post">'+object.get("objectId")+'</div>\
+                                      <ons-list-item>\
+                                        <div class="left">\
+                                            <img class="list-item__thumbnail" src="photo/defaultIcon.png">\
+                                          </div>\
+                                          <div class="center">\
+                                            <div class="list-item__title"><b>'+object.get("userName")+'</b></div>\
+                                            <div class="my_post-time">'+creDate+'</div>\
+                                        </div>\
+                                        <div class="right">\
+                                          <span class="corner-button" modifier="quiet" onclick="showPopover(this,'+i+','+1+')">\
+                                            <ons-icon icon="md-more-vert"></ons-icon>\
+                                          </span>\
+                                        </div>\
+                                      </ons-list-item>\
+                                      <div style="text-align: center; position: relative;">\
+                                        <img class="post-image" id="ind_img'+i+'" src="photo/cameraIcon.png" onclick="gosnshyouji('+i+')">\
+                                      </div>\
+                                      <ons-list-item class="post-button-bar" modifier="nodivider">\
+                                        <div class="center" style="padding-top: 0px" onclick="gosnshyouji('+i+')">\
+                                          <div class="post-caption" style="overflow:hidden;height:30px"> '+ object.get("text") +'\
+                                        </div>\
+                                        </div>\
+                                        <div class="right corner-button bookmark">\
+                                          <ons-button class="post-button" modifier="quiet" onclick="like('+ i +')"><ons-icon icon="md-favorite" id="button-post-like-'+ i +'" class = "like"></ons-icon></ons-button>\
+                                        </div>\
+                                      </ons-list-item>\
+                                    </ons-card>');
+          }
+        }else if(page === "mysns"){
+          return ons.createElement('<ons-card class="post" style="margin-bottom:10px">\
                                     <div id="my_obj'+i+'" class="obj_post">'+object.get("objectId")+'</div>\
                                     <ons-list-item>\
                                       <div class="left">\
-                                          <img class="list-item__thumbnail" src="photo/sofa.png" >\
+                                          <img class="list-item__thumbnail" src="photo/defaultIcon.png" >\
                                         </div>\
                                         <div class="center">\
-                                          <div class="list-item__title"><b>'+userName+'</b></div>\
-                                          <div class="my_post-time">'+Date+'</div>\
+                                          <div class="list-item__title"><b>'+object.get("userName")+'</b></div>\
+                                          <div class="my_post-time">'+creDate+'</div>\
                                       </div>\
                                       <div class="right">\
-                                        <span class="corner-button" modifier="quiet" onclick="showPopover(this,'+i+')">\
+                                        <span class="corner-button" modifier="quiet" onclick="showPopover(this,'+i+','+1+')">\
                                           <ons-icon icon="md-more-vert"></ons-icon>\
                                         </span>\
                                       </div>\
                                     </ons-list-item>\
                                     <div style="text-align: center; position: relative;">\
-                                      <img class="post-image" id="my_img'+i+'" src=""  onclick="gosnshyouji('+i+')">\
+                                      <img class="post-image" id="my_img'+i+'" src="photo/cameraIcon.png"  onclick="gosnshyouji('+i+')">\
                                     </div>\
                                     <ons-list-item class="post-button-bar" modifier="nodivider">\
-                                      <div class="center" style="padding-top: 0px">\
+                                       <div class="center" style="padding-top: 0px" onclick="gosnshyouji('+i+')">\
+                                        <div class="post-caption"> '+ object.get("text").substring(0,35) +'\
+                                      </div>\
+                                      <div class="center" style="opacity:0.5;position:absolute;margin-left:68%;margin-top:10px">\
+                                        詳細>>\
                                       </div>\
                                       <div class="right corner-button bookmark">\
-                                        <ons-button class="post-button" modifier="quiet" onclick="like('+ i +')"><ons-icon icon="md-favorite-outline" id="button-post-like-'+ i +'" ></ons-icon></ons-button>\
                                       </div>\
                                     </ons-list-item>\
-                                    <div class="post-like-info"><b></b> </div>\
-                                    <div class="post-caption"> '+ text +' </div>\
                                   </ons-card>');
         }
       },
@@ -137,7 +217,7 @@ function snsMake(res,snsList,page){
         return post;
       }
   };
-  //snsList.refresh();
+  // snsList.refresh();
 }
 
   function downloadImage(picture,reader){
@@ -156,19 +236,53 @@ function snsMake(res,snsList,page){
           })
     }
 
-    //snsの右上のポップアップメニュー表示切替
-    var showPopover = function(target,num) {
-      sns_num = num;
-      document
-        .getElementById('popover')
-        .show(target,num);
-    };
+    //ハートを押した時の処理
+    function like(num) {
+      var like_f = true; //addlikeSns制御
+      if(login_f){
+        // like(cssが付与)されているなら
+        if (document.getElementById("button-post-like-"+num).classList.contains("like")) {
+          document.getElementById("button-post-like-"+num).classList.remove("md-favorite","like");
+          document.getElementById("button-post-like-"+num).classList.add("md-favorite-outline");
+          like_f = false;
+        // いないなら
+        } else {
+          document.getElementById("button-post-like-"+num).classList.remove("md-favorite-outline");
+          document.getElementById("button-post-like-"+num).classList.add("md-favorite","like");
+          login_f = true;
+          //document.getElementById("post-like-"+num).style.opacity = 1;
 
-    var hidePopover = function() {
-      document
-        .getElementById('popover')
-        .hide();
-    };
+            //ハートを押したときに一定時間大きなハートを出す処理 未使用
+          /*setTimeout(function(){
+            document.getElementById("post-like-"+num).style.opacity = 0;
+          }, 600);*/
+        }
+        addLikeSns(like_f,num);
+      }
+    }
+
+    function addLikeSns(like_f,num){
+      // ログイン中のユーザー情報獲得
+      var currentUser = ncmb.User.getCurrentUser();
+
+      acountData
+      .equalTo("userName",currentUser.userName) // アカウントテーブルからユーザー名でデータ検索
+      .fetch()
+      .then(function(res){
+        var objId = document.getElementById("ind_obj"+num).innerHTML; //投稿の中に隠してあるオブジェクトIDを取得
+        //console.log("before "+res.likeSns);
+
+        //お気に入りしていないなら配列に追加
+        //しているなら削除
+        if(like_f){
+          res.likeSns.push(objId);
+        }else{
+          res.likeSns = res.likeSns.filter(res => res !== objId); //配列 + filter(変数名 検索条件 変数名 検索条件 検索対象);
+        }
+        //console.log("after "+res.likeSns);
+        return res.update();　//ニフクラデータ更新　配列の場合 .set でデータをセットしなくていい
+      });
+    }
 
      function gosnshyouji(i){
         // sessionStorage にデータを保存する
@@ -177,3 +291,5 @@ function snsMake(res,snsList,page){
         sns_post = i;
         fn.pushPage('snshyouji.html');
     }
+
+    
